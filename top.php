34891c12bae01dd03daa9509259e516df64e97d9
OhgamiKanako <o2605.rf8k21d5ptr@gmail.com>
2021-03-08 23:50:01 +0900

    03/08_home

--- top.php
+++ top.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
     <style>
@@ -14,6 +15,7 @@
     <title>Document</title>
 </head>
 <body>
+<img src="images/4191096_m.jpg" alt="top画">
 <!-- カレンダー -->
 <div class="container">
     <h3><a>&lt;</a>&nbsp;&nbsp;2018年 7月&nbsp;&nbsp;<a>&gt;</a></h3>
