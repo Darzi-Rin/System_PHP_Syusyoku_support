2fa7d72ef7686b41f5f9f338a538cc0e9d3a4ced
yamutya <ksw2070374@stu.o-hara.ac.jp>
2021-03-08 11:24:20 +0900

    コミット練習

--- login.php
+++ login.php
@@ -1,1 +1,17 @@
-
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>名前</p>
+    <input type="text" id="name">
+    <p>パスワード</p>
+    <input type="text" id="pass">
+    <p><a href="login_check_true.html"><input type="submit" value="login"></a></p>
+    <P><a href="login_check_false.html"></a></P>
+</body>
+</html>
