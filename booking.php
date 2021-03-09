4c4223b1aefaf08ebee1ded984a87fbd6da86737
Kouki-N <K.nakano305@gmail.com>
2021-03-09 12:25:19 +0900

    htmlは完成

--- booking.php
+++ booking.php
@@ -7,21 +7,61 @@
     <title>Document</title>
 </head>
 <body>
-    <div id="datepicker"></div><br>
-    時間を選択してください<br>
-        <select name="time">
-        <option value=""> 選択してください</option>
-        <option value="">9:00～</option>
-        <option value="">10:00～</option>
-        <option value="">11:00～</option>
-        <option value="">13:00～</option>
-        <option value="">14:00～</option>
-        <option value="">15:00～</option>
-        <option value="">16:00～</option>
-        <option value="">17:00～</option>
-        </select><br><br><br>
+    <table border="1">
+        <div id="datepicker">
+            <tr>
+                <th>開催日</th><th>時間</th><th>場所</th><th>備考（会場）</th><th>予約</th>
+            </tr>
+            <tr>
+                <td>2022/03/09(月)</td>
+                <td>10:00~13:00</td>
+                <td>東京</td>
+                <td>Webセミナー</td>
+                <td><input type="radio" name="check" value=""></td>
+            </tr>
+            <tr>
+                <td>2022/03/10(火)</td>
+                <td>10:00~13:00</td>
+                <td>東京</td>
+                <td>Webセミナー</td>
+                <td><input type="radio" name="check" value=""></td>
+            </tr>
+            <tr>
+                <td>2022/03/11(水)</td>
+                <td>10:00~13:00</td>
+                <td>東京</td>
+                <td>Webセミナー</td>
+                <td><input type="radio" name="check" value=""></td>
+            </tr>
+            <tr>
+                <td>2022/03/12(木)</td>
+                <td>10:00~13:00</td>
+                <td>東京</td>
+                <td>Webセミナー</td>
+                <td><input type="radio" name="check" value=""></td>
+            </tr>
+            <tr>
+                <td>2022/03/13(金)</td>
+                <td>10:00~13:00</td>
+                <td>東京</td>
+                <td>Webセミナー</td>
+                <td><input type="radio" name="check" value=""></td>
+            </tr>
+            <tr>
+                <td>2022/03/16(月)</td>
+                <td>10:00~13:00</td>
+                <td>東京</td>
+                <td>Webセミナー</td>
+                <td><input type="radio" name="check" value=""></td>
+            </tr>
+        </div>
+    </table>
+   
         
 <section>
+    <div class="customer">
+        <h3>お客様情報</h3>
+    </div>
     <form id="entry">
         <dl>
             <dt>お名前 <span class="must">※</span></dt>
@@ -30,15 +70,13 @@
             <dd><input type="text" name="email"></dd>
             <dt>TEL <span class="must">※</span></dt>
             <dd><input type="text" name="tel"></dd>
-            <dt>パスワード <span class="must">※</span></dt>
-            <dd><input type="text" name="password"></dd>
             <dt>性別 <span class="must"></span></dt>
-            <dd><input type="radio" name="sex" value="" />男
-                <input type="radio" name="sex" value="" />女
+            <dd><input type="radio" name="human" value="" />男
+                <input type="radio" name="human" value="" />女
             </dd>
         </dl>
         <p id="submit_button_cover">
-            <input type="submit" id="submit_button" value="予約する">
+            <input type="button" onclick="location.href='./booking_end.html'" value="予約する">
         </p>
     </form>
 </section>
