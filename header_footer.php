046335c25317a8d560475fa9bff8ac8d0ecbaed8
Darzi-Rin <ksw2070039@stu.o-hara.ac.jp>
2021-03-09 10:29:23 +0900

    header_hooter.phpのhtml完成

---
+++ header_footer.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang='ja'>
+<head>
+  <meta charset='UTF-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <link rel='stylesheet' href='css/style_home.css'>
+  <link rel='stylesheet' href='js/top.js'>
+  <title>Document</title>
+</head>
+<body>
+<!-- header -->
+<header>
+  <nav>
+    <ul class="color">
+      <li class="current"><a class="items" href="top.php">トップページ</a></li>
+      <li><a class="items" href="new_member.php">会員登録</a></li>
+      <li><a class="items" href="mypage.php">マイページ</a></li>
+      <li><a class="items" href="login.php">ログイン</a></li>
+    </ul>
+  </nav>
+</header>
+<!-- footer -->
+<footer>
+  <ul class="footer-menu">
+    <li id="pagetop"><a href="#" class="page-top">トップへ戻る ｜</a></li>
+    <li><a href="new_member.php">会員登録 ｜</li>
+    <li><a href="mypage.php">マイページ ｜</li>
+    <li><a href="login.php">ログイン</li>
+  </ul>
+  <p>© sample_site</p>
+</footer>
+</body>
+</html>
