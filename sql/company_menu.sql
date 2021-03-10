8d54f8212b4be1cccabd0aa36e50069a3d1f0902
OhgamiKanako <o2605.rf8k21d5ptr@gmail.com>
2021-03-10 10:21:54 +0900

    03/10_shcool

---
+++ sql/company_menu.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2021-03-10 02:20:33
+-- サーバのバージョン： 10.4.11-MariaDB
+-- PHP のバージョン: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `company_menu`
+--
+
+CREATE TABLE `company_menu` (
+  `id` int(10) NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8mb4 NOT NULL,
+  `date` varchar(30) CHARACTER SET utf8mb4 NOT NULL,
+  `company` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `category` int(10) NOT NULL,
+  `explan` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+  `reserv` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- テーブルのデータのダンプ `company_menu`
+--
+
+INSERT INTO `company_menu` (`id`, `name`, `date`, `company`, `category`, `explan`, `reserv`) VALUES
+(0, '富山ソフト(株)', '2021-03-12', 'ソフトウェア/情報処理/インターネット関連', 1, '“独立系ITソリューションベンダーとして、ソフト開発、システム開発・導入・保守・運用、コンサルティングなど幅広く提供。国内に留まらず、海外市場へも業務展開”', NULL);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
