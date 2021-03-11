fa3bf1fd96740c3be750f797d3738aa1af2210e2
Darzi-Rin <ksw2070039@stu.o-hara.ac.jp>
2021-03-11 12:25:21 +0900

    php_session完成

--- mypage.php
+++ mypage.php
@@ -1,51 +1,54 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang='ja'>
 
 <head>
   <meta charset='UTF-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <link rel='stylesheet' href='css/style_home.css'>
   <link rel='stylesheet' href='css/style_mypage.css'>
   <link rel='stylesheet' href='js/top.js'>
   <title>マイページ</title>
 </head>
 <!-- header -->
 <header>
-  <nav>
-    <ul class="color">
-      <li><a class="items" href="top.php">トップページ</a></li>
-      <li><a class="items" href="new_member.php">会員登録</a></li>
-      <li class="current"><a class="items" href="mypage.php">マイページ</a></li>
-      <li><a class="items" href="login.php">ログイン</a></li>
-    </ul>
-  </nav>
+  <?php require_once 'header.php'; ?>
 </header>
 
 <body>
   <div class='personal_infomation'>
     <h2>個人情報</h2>
-    <p>テストテストテスト</p>
-    <p>テストテストテスト</p>
-    <p>テストテストテスト</p>
-    <p>テストテストテスト</p>
+    <?php
+    if (!isset($_SESSION['user'])) {
+      echo '個人情報を確認するにはログインしてください。';
+    } else {  //正常処理 ?>
+    <p>お名前：<?= $_SESSION['user']['name'] ?></p>
+    <p>パスワード：<?= $_SESSION['user']['password'] ?></p>
+    <p>年齢：<?= $_SESSION['user']['age'] ?></p>
+		<p>ご住所：<?= $_SESSION['user']['street_address'] ?></p>
+    <p>メールアドレス：<?= $_SESSION['user']['mail'] ?></p>
+    <?php
+    }
+    ?>
   </div>
   <br>
-  <div class='yoyaku'>
+  <div class='yoyakukanri'>
     <h2>予約管理</h2>
-    <p>テストテストテスト</p>
-    <p>テストテストテスト</p>
-    <p>テストテストテスト</p>
-    <p>テストテストテスト</p>
+    <?php
+    if (!isset($_SESSION['yoyaku'])) {
+      echo '予約管理を確認するにはログインしてください。';
+    } else {  //正常処理 ?>
+    <p>お名前：<?= $_SESSION['yoyaku']['name'] ?></p>
+    <p>日付：<?= $_SESSION['yoyaku']['date'] ?></p>
+    <p>会社名：<?= $_SESSION['yoyaku']['company'] ?></p>
+    <?php
+    }
+    ?>
   </div>
 </body>
 <!-- footer -->
 <footer>
-  <ul class="footer-menu">
-    <li id="pagetop"><a href="top.php" class="page-top">トップへ戻る ｜</a></li>
-    <li><a href="new_member.php">会員登録 ｜</li>
-    <li><a href="mypage.php">マイページ ｜</li>
-    <li><a href="login.php">ログイン</li>
-  </ul>
-  <p>© sample_site</p>
+<?php require_once 'footer.php'; ?>
 </footer>
 
 </html>
