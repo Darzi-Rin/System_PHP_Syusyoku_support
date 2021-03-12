e7e2cc7b244fa50f6a326ff8a263d91ea4b53348
Kouki-N <K.nakano305@gmail.com>
2021-03-12 12:06:46 +0900

    cssはまだ

--- booking.php
+++ booking.php
@@ -1,82 +1,48 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style_home.css">
+    <link rel="stylesheet" href="css/style_booking.css">
+    <link rel="stylesheet" href="js/top.js">
     <title>Document</title>
 </head>
+<header>
+    <?php require_once 'header.php'; ?>
+</header>
 <body>
-    <table border="1">
-        <div id="datepicker">
-            <tr>
-                <th>開催日</th><th>時間</th><th>場所</th><th>備考（会場）</th><th>予約</th>
-            </tr>
-            <tr>
-                <td>2022/03/09(月)</td>
-                <td>10:00~13:00</td>
-                <td>東京</td>
-                <td>Webセミナー</td>
-                <td><input type="radio" name="check" value=""></td>
-            </tr>
-            <tr>
-                <td>2022/03/10(火)</td>
-                <td>10:00~13:00</td>
-                <td>東京</td>
-                <td>Webセミナー</td>
-                <td><input type="radio" name="check" value=""></td>
-            </tr>
-            <tr>
-                <td>2022/03/11(水)</td>
-                <td>10:00~13:00</td>
-                <td>東京</td>
-                <td>Webセミナー</td>
-                <td><input type="radio" name="check" value=""></td>
-            </tr>
-            <tr>
-                <td>2022/03/12(木)</td>
-                <td>10:00~13:00</td>
-                <td>東京</td>
-                <td>Webセミナー</td>
-                <td><input type="radio" name="check" value=""></td>
-            </tr>
-            <tr>
-                <td>2022/03/13(金)</td>
-                <td>10:00~13:00</td>
-                <td>東京</td>
-                <td>Webセミナー</td>
-                <td><input type="radio" name="check" value=""></td>
-            </tr>
-            <tr>
-                <td>2022/03/16(月)</td>
-                <td>10:00~13:00</td>
-                <td>東京</td>
-                <td>Webセミナー</td>
-                <td><input type="radio" name="check" value=""></td>
-            </tr>
-        </div>
-    </table>
-   
-        
+    <div id="datepicker">
+        <form action="booking_end.php" method="post">
+	        <table>
+	            <tr><th><label for="name">お名前:</label></th>
+	                <td><input type="text" name="name" value="" ></td>
+	            </tr>
+		        <tr><th><label for="date">日時:</label></th>
+		            <td><input type="date" name="date" value="" ></td>
+		        </tr>
+		        <tr><th><label for="company">会社名:</label></th>
+		            <td><input type="text" name="company" value="" ></td>
+		        </tr>
+	        </table>
+        </form>
+    </div>   
 <section>
     <div class="customer">
-        <h3>お客様情報</h3>
+        <h3>個人情報</h3>
+        <p>お名前:<?= $_SESSION['user']['name'] ?></p>
+        <p>パスワード:<?= $_SESSION['user']['password'] ?></p>
+        <p>住所:<?= $_SEESION['user']['street_addrees'] ?></p>
+        <p>メールアドレス:<?= $_SESSION['user']['mail'] ?></p>
+        <p>年齢:<?= $_SESSION['user']['age'] ?></p>
     </div>
-    <form id="entry">
-        <dl>
-            <dt>お名前 <span class="must">※</span></dt>
-            <dd><input type="text" name="name"></dd>
-            <dt>メールアドレス <span class="must">※</span></dt>
-            <dd><input type="text" name="mail"></dd>
-            <dt>パスワード <span class="must">※</span></dt>
-            <dd><input type="text" name="password"></dd>
-            <dt>年齢 <span class="must">※</span></dt>
-            <dd><input type="text" name="age"></dd>
-        </dl>
         <p id="submit_button_cover">
-            <input type="button" onclick="location.href='./booking_end.html'" value="予約する">
+            <input type="button" onclick="location.href='./booking_end.php'" value="予約する">
         </p>
-    </form>
 </section>
 </body>
+<footer>
+    <? require_once 'footer.php'; ?>
+</footer>
 </html>
